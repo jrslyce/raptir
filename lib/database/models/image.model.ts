1c10cfdc8a1a26d16fb3e4fd44b934f070d766d8
Jared Steffes <fakebusy@MacBook-Pro.local>
2024-02-21 13:55:05 -0600

    initial commit

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,41 @@
+import { Schema, model }  from "mongoose";
+
+export interface IImage extends Document {
+    title: string;
+    transfomationType: string;
+    publicID: string;
+    secureUrl: string;
+    width?: number;
+    height?: number;
+    config?: object;
+    transformationUrl?: string;
+    aspectRation?: string;
+    color?: string;
+    prompt?: string;
+    author: {
+        _id: string;
+        firstName: string;
+        lastName: string;
+    };
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const ImageSchema = new Schema({
+    title: { type: String, required: true },
+    transfomationType: { type: String, required: true },
+    publicID: { type: String, required: true },
+    secureUrl: { type: URL, required: true },
+    width: { type: Number },
+    height: { type: Number },
+    config: { type: Object },
+    transformationUrl: { type: URL },
+    aspectRation: { type: String },
+    color: { type: String },
+    prompt: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+const Image = models?.Image || model('Image', ImageSchema);
